23431c5a6ffa82918cf9fba4d0d37fc4fca8e9b8
Saymon21 <saymon@hub21.ru>
2013-05-19 01:41:50 +0400

    New version

---
+++ smsrulib.lua
@@ -0,0 +1,286 @@
+-- ***************************************************************************
+-- smsrulib - Lua bindings for the sms.ru API. 
+-- Lets you send messages, check their status, find their cost etc.
+
+-- Copyright © 2013 Denis Khabarov aka 'Saymon21'
+-- E-Mail: saymon at hub21 dot ru (saymon@hub21.ru)
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License version 3
+-- as published by the Free Software Foundation.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- ***************************************************************************
+local base = _G
+module('smsrulib')
+_DEBUG = false
+_HTTP_TIMEOUT = 10 -- timeout for all luasocket I/O operations
+_VERSION = '0.1'
+local httplib = base.require 'socket.http'
+local urllib = base.require 'socket.url'
+httplib.TIMEOUT = _HTTP_TIMEOUT
+smsru = {}
+--httplib.PROXY = ""
+--httplib.USERAGENT = 'smsrulib_version_'.._VERSION..'_(http://opensource.hub21.ru/smssend.lua)'
+-- *************************************************************
+local function tracert(...)
+	if _DEBUG then
+		base.print(...)
+	end
+end
+
+local function isset(d)
+	if d and base.type(d) == 'string' then
+		return true
+	elseif d and base.type(d) == "number" and d == 1 then
+		return true
+	end
+end
+
+local function ltable2http_arg(t)
+	local s,c = '',0
+	for i,v in base.pairs(t) do
+		if isset(v) then
+			c = c +1
+			tracert(i,v)
+			if c ~= 1 then
+				s=(('%s&%s=%s'):format(s, i ,urllib.escape(v)))
+			else 
+				s =(('%s%s=%s'):format(s, i, urllib.escape(v)))
+			end
+		end
+	end
+	return s
+end
+
+function service_call(method, args)
+	local methods={ ['sms/send'] = true, ['auth/get_token'] = true, ['auth/check'] = true, 
+					['sms/status'] = true, ['sms/cost'] = true,
+					['my/balance'] = true, ['my/limit'] = true,
+					['stoplist/add'] = true, ['stoplist/del'] = true,}
+	if not method or not methods[method] then
+		base.error('Method not found or is not supported')
+		return
+	end
+	local url=''
+	if base.type(args) == 'table' then
+		url = (('http://sms.ru/%s?%s'):format(method, ltable2http_arg(args)))
+	else
+		url = (('http://sms.ru/%s'):format(method))
+	end
+	tracert('@service_call','request: ', url)
+	local res, err = httplib.request(url)
+	if not res then
+		tracert('@service_call: ', err)
+		return nil, (('socket.http error: %s'):format(err))
+	end
+	tracert('@service_call:', 'Recv: ', res)
+	local result = res:split('\n')
+	if result then
+		return result
+	end
+end
+
+function base.string:split(separator, max, bregexp)
+    base.assert(separator ~= '')
+    base.assert(max == nil or max >= 1)
+
+    local record = {}
+
+    if self:len() > 0 then
+        local plain = not bregexp
+        max = max or -1
+
+        local field=1 start=1
+        local first,last = self:find(separator, start, plain)
+        while first and max ~= 0 do
+            record[field] = self:sub(start, first-1)
+            field = field+1
+            start = last+1
+            first,last = self:find(separator, start, plain)
+            max = max-1
+        end
+        record[field] = self:sub(start)
+    end
+    return record
+end
+
+
+function init(myphone, password)
+	mt = base.setmetatable({login = myphone, password = password}, {__index = smsru })
+	if mt:init() then	
+		return mt
+	end
+end
+
+function smsru:init()
+	if self:test_auth() == 100 then
+		return true
+	end
+end
+
+function smsru:get_crypt_key()
+	self.token = self:get_token()
+	local bcrypto,cryptlib = base.pcall(base.require, 'crypto') -- http://luacrypto.luaforge.net/
+	if bcrypto then
+		tracert('@get_crypt_key: using \'crypto\' library')
+		return cryptlib.digest('SHA512',self.password..self.token)
+	end
+	local bsha2, shalib = base.pcall(base.require,'sha2') -- http://code.google.com/p/sha2/
+	if bsha2 then
+		tracert('@get_crypt_key: using \'sha2\' library')
+		return shalib.sha512hex(self.password..self.token)
+	end
+end
+
+function smsru:get_token()
+	result = service_call('auth/get_token')
+	if result then 
+		return result[1]
+	end
+end
+
+function smsru:send(to, text, from, time, translit, test)	
+	if test then
+		test="1"
+	elseif translit then
+		translit = "1"
+	end
+	
+	local result = service_call('sms/send', {
+					login = self.login,
+					sha512 = self:get_crypt_key(), 
+					token = self.token,
+					to = to,
+					text = text, 
+					from = from, 
+					time = time, 
+					translit = translit, 
+					test = test,
+					partner_id = 3805})
+	if result and base.type(result) == 'table' then
+		if base.tonumber(result[1]) == 100 then
+			return true, result[2]
+		else
+			return nil, result[1] and base.tonumber(result[1])
+		end
+	end
+end
+
+function smsru:status(id)
+	result = service_call('sms/status', {
+				login = self.login, 
+				sha512 = self:get_crypt_key(), 
+				token = self.token, 
+				id = id})
+	if result then
+		return result[1] and base.tonumber(result[1])
+	end
+end
+
+function smsru:cost(to, message)
+	local result = service_call('sms/cost',{
+					login = self.login,
+					sha512 = self:get_crypt_key(),
+					token = self.token,
+					text = message,
+					to = to})
+	if result and base.type(result) == 'table' then
+		if base.tonumber(result[1]) == 100 then
+			return true, result
+		else
+			return nil, result[1] and base.tonumber(result[1])
+		end 
+	end	
+end
+
+function smsru:balance()
+	local result = service_call('my/balance',{
+					login = self.login,
+					sha512 = self:get_crypt_key(),
+					token = self.token})
+	if result and base.type(result) == 'table' then
+		if base.tonumber(result[1]) == 100 then
+			return true, result[2]
+		else 
+			return nil, result[1] and base.tonumber(result[1])
+		end
+	end
+end
+
+function smsru:limit()
+	local result = service_call('my/limit',{
+					login = self.login,
+					sha512 = self:get_crypt_key(),
+					token = self.token})
+	if result and base.type(result) == 'table' then
+		
+		if base.tonumber(result[1]) == 100 then
+			return true, result
+		else 
+			return nil, result
+		end
+	end
+end
+
+function smsru:stoplist_add(phone, reason)
+	local result = service_call('stoplist/add',{
+		login = self.login,
+		sha512 = self:get_crypt_key(),
+		token = self.token,
+		stoplist_phone = phone,
+		stoplist_text = reason,
+	})
+	if result and base.type(result) == 'table' then
+		if base.tonumber(result[1]) == 100 then
+			return true
+		elseif base.tonumber(result[1]) == 202 then
+			return false
+		end
+	end
+end
+
+function smsru:stoplist_del(phone)
+	local result = service_call('stoplist/del',{
+		login = self.login,
+		sha512 = self:get_crypt_key(),
+		token = self.token,
+		stoplist_phone = phone
+	})
+	if result and base.type(result) == 'table' then
+		if base.tonumber(result[1]) == 100 then
+			return true
+		elseif base.tonumber(result[1]) == 202 then
+			return false
+		end
+	end
+end
+
+function smsru:stoplist_get() -- TODO: FIXME
+	local result = service_call('stoplist/get',{
+		login = self.login,
+		sha512 = self:get_crypt_key(),
+		token = self.token,
+	})
+	
+	if result and base.type(result) == 'table' then
+		return result
+	end
+end
+
+function smsru:test_auth()
+	local result = service_call('auth/check', {
+		login = self.login,
+		sha512 = self:get_crypt_key(),
+		token = self.token
+	})
+	if result and base.type(result) == 'table' then
+		return result[1] and base.tonumber(result[1])
+	end
+end
--- smssend.lua
+++ smssend.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua5.1
 -- ***************************************************************************
--- smssend - smssend is a program to send SMS messages from the commandline.
+-- smssend - is a program to send SMS messages from the commandline. (Lua Version)
 
 -- Copyright © 2013 Denis Khabarov aka 'Saymon21'
 -- E-Mail: saymon at hub21 dot ru (saymon@hub21.ru)
@@ -11,47 +11,85 @@
 
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 -- GNU General Public License for more details.
 
 -- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 -- ***************************************************************************
+local smsrulib = require('smsrulib')
+local tArgs = {}
+local _EXIT_SUCCESS = 0
+local _EXIT_SMSRU_ERR = 1
+local _EXIT_ERRUSE =  2
+local _MYVERSION = '0.2-beta'
 
--- ATTENTION!!! This is beta version!!!
--- Recommended to use smssend.py from the repository - http://opensource.hub21.ru/smssend/
--- Tested only on Debian GNU/Linux & Lua 5.1.4
-
-tArgs = {}
 function show_help()
-print("smssend is a program to send SMS messages from the commandline.\
-Using API service http://sms.ru\
-Copyright © 2009-2012 by Denis Khabarov aka 'Saymon21'\
-E-Mail: saymon at hub21 dot ru (saymon@hub21.ru)\
-\nOptional arguments:\
-\t--help\t\tshow this help message and exit\
-\t--apiid\tVALUE\t\tAPI ID (optional)\
-\t--to PHONENUMBER\t\tTelephone number where send sms message (required)\
-\t--message MESSAGE\t\tRead the message from this argument value instead of stdin (optional)\
-\t--from VALUE\t\tSender name (optional)\
-\t--time VALUE\t\tSend time (optional)\
-\nReturned codes:\n\
-\t0\t- Message send successful\
-\t1\t- Service has retured error message\
-\t2\t- HTTP Error\
-\t3\t- Error for usage this tool\n\
-Default API ID are read from the files:\
-\tLinux: $HOME/.smssendrc\
-\tWindows: %USERPROFILE%/.smssendrc\n\
-Example usage:\
-\techo \"Hello world\" | smssend.lua --apiid=yourapiid --to=target_phone_number")
+	print('\n\tsmssend is a program to send SMS messages from the commandline.\n\tUsing API service http://sms.ru\
+\tCopyright © 2009-2012 by Denis Khabarov aka \'Saymon21\'\n\tE-Mail: saymon at hub21 dot ru (saymon@hub21.ru)\
+\tHomepage: http://opensource.hub21.ru/smssend.lua\n\n\tOptions:')
+	print('\t--help', '- show this help')
+	print('\t--version', '- show version')
+	print('\t--login', '- login at service sms.ru')
+	print('\t--password', '- password at service sms.ru')
+	print('\t--action', '- run action (available actions: send, status, cost, balance, limit, stoplistadd, stoplistdel)')
+	print('\t--to', '- send message to this phone number')
+	print('\t--message', '- don\'t read stdin. Send text from this argument')
+	print('\t--from', '- sender name')
+	print('\t--time', '- set send time (in unixtime format)')
+	print('\t--translit', '- convert message to translit')
+	os.exit(_EXIT_SUCCESS)
+end
+
+function cliarg_parser()
+	if arg then
+		local available_args = {
+			['login'] = true, ['password'] = true, 
+			['action'] = true, ['to'] = true, 
+			['message'] = true, ['from'] = true, 
+			['time'] = true, ['help']= true,
+			['translit'] = true, ['version'] = true,
+			--['debug'] = true,
+ 		}
+ 		for _, val in ipairs(arg) do
+			if val:find('=', 1, true) then
+				local name, value = val:match('%-%-(.-)=(.+)')
+				if name and value and available_args[name:lower()] then
+					tArgs[name:lower()] = value
+				else
+					print(('Unknown commandline argument used: %s\nusage: %s --help'):format(val,arg[0]))
+					os.exit(_EXIT_ERRUSE)
+				end
+			else
+				name = val:match('%-%-(.+)')
+				if name and  available_args[name:lower()] then
+					tArgs[name:lower()] = true
+				else
+					print(('Unknown commandline argument used: %s\nusage: %s --help'):format(val,arg[0]))
+					os.exit(_EXIT_ERRUSE)
+				end
+			end
+		end
+		if not tArgs.action then
+			tArgs.action = 'send' -- Set default action. (Send SMS message)
+		else 
+			tArgs.action = tArgs.action:lower()
+		end
+		if tArgs.help then
+			show_help()
+		end
+		if tArgs.version then
+			print(('smssend.lua version: %s'):format(_MYVERSION))
+			os.exit(_EXIT_SUCCESS)
+		end
+	end
 end
 
 function get_os()
 	local path_separator = package.config:sub(1,1)
-	if path_separator == "/" then
+	if path_separator == '/' then
 		return 1 -- Unix
-	elseif path_separator == "\\" then
+	elseif path_separator == '\\' then
 		return 2 -- Win
 	end
 end
@@ -59,130 +97,337 @@ end
 function get_home_path()
 	local _os = get_os()
 	if _os == 1 then
-		home = os.getenv("HOME")
+		home = os.getenv('HOME')
 	elseif _os == 2 then
 		home = os.getenv("USERPROFILE")
-	else os.exit(3)
+	else 
+		os.exit(_EXIT_ERRUSE)
 	end
 	return home
 end
 
-function get_api_id()
+function read_smssendrc()
 	local home = get_home_path()
 	if home then
-		h=io.open(home.."/.smssendrc","r")
+		h=io.open(home..'/.smssendrc','r')
 		if h then
 			data=h:read()
 		end
 	end
 	if data then
-		return data
-	else 
-		if tArgs["apiid"] then
-			return tArgs["apiid"]
-		else
-			print("Error. See --help")
-			os.exit(3)
+		local login, password = data:match('(%d+):(.+)')
+		if login and password then
+			return login, password	 
 		end
 	end		
 end
 
+function getpass()
+    local stty_ret = os.execute('stty -echo 2>/dev/null')
+    if stty_ret ~= 0 then
+        io.write('\027[08m') -- ANSI 'hidden' text attribute
+    end
+    local ok, pass = pcall(io.read, "*l")
+    if stty_ret == 0 then
+        os.execute("stty sane")
+    else
+        io.write("\027[00m")
+    end
+    io.write("\n")
+    if ok then
+        return pass
+    end
+end
+
+function getchar(n)
+    local stty_ret = os.execute("stty raw -echo 2>/dev/null")
+    local ok, char
+    if stty_ret == 0 then
+        ok, char = pcall(io.read, n or 1)
+        os.execute("stty sane")
+    else
+        ok, char = pcall(io.read, "*l")
+        if ok then
+            char = char:sub(1, n or 1)
+        end
+    end
+    if ok then
+        return char
+    end
+end
+
+function show_yesno(prompt)
+    io.write(prompt, " ")
+    local choice = getchar():lower()
+    io.write("\n");
+    if not choice:match("%a") then
+        choice = prompt:match("%[.-(%U).-%]$")
+        if not choice then 
+        	return nil 
+        end
+    end
+    return (choice == "y")
+end
+
+function read_password()
+    local password
+    while true do
+        io.write("Enter password: ")
+        password = getpass();
+        if not password then
+            print("No password - cancelled")
+            return
+        end
+        io.write("Retype password: ")
+        if getpass() ~= password then
+            if not show_yesno('Passwords did not match, try again? [Y/n]') then
+                return
+            end
+        else
+            break
+        end
+    end
+    return password
+end
+
+function term_get_login()
+	local login 
+	io.write('Enter login: ')
+	local ok, login = pcall(io.read, "*l")
+	if ok then
+		return login		
+	elseif not ok then
+		print('No login cancelled')
+		return nil
+	end
+end
+
 function get_msg()
-	if tArgs['message'] then
-		return tArgs['message']
+	if tArgs.message then
+		return tArgs.message
 	else
 		return io.stdin:read()
 	end
 end
 
-
-function cliarg_parser()
-	if arg then
-		local available_args = {
-			["apiid"] = true, ["to"] = true, ["message"] = true, ["from"] = true, ["time"] = true, ["help"]= true,
- 		}
- 		for _, val in ipairs(arg) do
-			if val:find("=", 1, true) then
-				local name, value = val:match("%-%-(.-)=(.+)")
-				--print("Commandline argument: "..tostring(name).." = "..tostring(value).."\n")
-				if name and value and available_args[name:lower()] then
-					tArgs[name:lower()] = value
-					--print("[parser debug]: Commandline argument: "..tostring(name).." = "..tostring(value))
-				else
-					print("Unknown commandline argument used: "..val.."\nusage: "..arg[0].."--help")
-					os.exit(3)
-				end
+function main()
+	cliarg_parser()
+	if not tArgs.login or not tArgs.password then
+		login, password = read_smssendrc()
+		if login and password then
+			tArgs.login = login
+			tArgs.password = password
+		else 
+			login = term_get_login()
+			if not login then
+				os.exit(_EXIT_ERRUSE)
+			else 
+				tArgs.login = login
+			end
+			password = read_password()
+			if not password then
+				print('No password - cancelled')
+				os.exit(_EXIT_ERRUSE)
 			else
-				name = val:match("%-%-(.+)")
-				if name and  available_args[name:lower()] then
-					tArgs[name:lower()] = true
-					--print("[parser debug]: Commandline argument: "..tostring(name).." is set as TRUE ") -- debug
-				else
-					print("Unknown commandline argument used: "..val.."\nusage: "..arg[0].." --help")
-					os.exit(3)
-				end
+				tArgs.password = password
 			end
 		end
 	end
-	
-	if tArgs["help"] then
-		show_help() -- Show help
-		os.exit(0)
+	sms_client = smsrulib.init(tArgs.login, tArgs.password)
+	if not sms_client or type(sms_client) ~= 'table' then
+		print('Error. Unable to initialize client.')
+		os.exit(3)
 	end
-	if not tArgs["to"] then
-		print("Error. See --help")
-		os.exit(3)
-	elseif not tArgs["to"]:find("^%d+$") then
-		print("Invalid argument for --to.")
-		os.exit(3)
+	if tArgs.action == 'send' then
+		sms_send()
+	elseif tArgs.action == 'status' then
+		sms_status()
+	elseif tArgs.action == 'cost' then
+		sms_cost()
+	elseif tArgs.action == 'balance' then
+		get_balance()
+	elseif tArgs.action == 'limit' then
+		get_limit()
+	elseif tArgs.action == 'stoplistadd' then
+		stoplist_add()
+	elseif tArgs.action == 'stoplistdel' then
+		stoplist_del()
+	else 
+		print('Unknown action.')
+		os.exit(_EXIT_ERRUSE)
 	end
 end
 
-
-function main()
-	print("ATTENTION!!! This is beta version!!!\nRecommended to use smssend.py from the repository - http://opensource.hub21.ru/smssend/\n\n")
-	urllib = require"socket.url"
-	httplib = require"socket.http"
-	cliarg_parser()
-	api_id = get_api_id()
-	if not api_id then
-		print(("Error for get api-id. Check %s/.smssendrc or see --help"):format(get_home_path()))
-		os.exit(1)
+function sms_status()
+	if not tArgs.message or type(tArgs.message) ~= 'string' then
+		print('Invalid value for argument \'message\'.')
+		os.exit(_EXIT_ERRUSE)
 	end
-	api_id = api_id:gsub("\r\n", "")
-	api_id = api_id:gsub("\n", "")
-	msg = get_msg()
-	if msg:len() == 0 then
-		print("Error. Please enter msg")
-		os.exit(1)
-	end
-	
-	local url=(("http://sms.ru/sms/send?api_id=%s&to=%s&text=%s&partner_id=3805"):format(api_id, tArgs["to"],  urllib.escape(msg)))
-	
-	if tArgs["from"] then
-		local url=(("%s&from=%s"):format(url, urllib.escape(tArgs["from"])))
-	elseif tArgs["time"] then
-		local url=(("%s&time=%d"):format(url, tonumber(tArgs["time"])))
-	end
-	
-	local res, err = httplib.request(url)
-	if not res and err then
-		os.exit(2)
-	end
-	local res=res:gsub("\n", " ")
-	local res=res:match("^(%d+)%s+")
+	local _STATUSES={
+	[-1] = 'Сообщение не найдено.',
+	[100] = 'Сообщение находится в нашей очереди',
+	[101] = 'Сообщение передается оператору',
+	[102] = 'Сообщение отправлено (в пути)',
+	[103] = 'Сообщение доставлено',
+	[104] = 'Не может быть доставлено: время жизни истекло',
+	[105] = 'Не может быть доставлено: удалено оператором',
+	[106] = 'Не может быть доставлено: сбой в телефоне',
+	[107] = 'Не может быть доставлено: неизвестная причина',
+	[108] = 'Не может быть доставлено: отклонено',
+	[200] = 'Неправильный api_id',
+	[210] = 'Используется GET, где необходимо использовать POST',
+	[211] = 'Метод не найден',
+	[220] = 'Сервис временно недоступен, попробуйте чуть позже.',
+	[300] = 'Неправильный token (возможно истек срок действия, либо ваш IP изменился)',
+	[301] = 'Неправильный пароль, либо пользователь не найден',
+	[302] = 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс)',
+	}
+	local res = sms_client:status(tArgs.message)
 	if res then
-		code=tonumber(res)
-		if code == 100 then
-			os.exit(0)
-		elseif not code == 100 then
-			os.exit(1)
-		end
-	else
-		os.exit(1)
+		print(('%s: %s'):format(tArgs.message,(_STATUSES[res] and _STATUSES[res] or res)))
+		os.exit(_EXIT_SUCCESS)	
 	end
 end
 
-if type(arg) == "table" then
+function sms_cost()
+	local msg = get_msg()
+	if not tArgs.to or not tArgs.to:match("^%d+$") then
+		print('Invalid value for argument \'to\'.')
+		os.exit(_EXIT_ERRUSE)
+	end
+	
+	local _STATUSES = {
+		[100] = 'Запрос выполнен. На второй строчке будет указана стоимость сообщения. На третьей строчке будет указана его длина.',
+		[200] = 'Неправильный api_id',
+		[202] = 'Неправильно указан получатель',
+		[207] = 'На этот номер нельзя отправлять сообщения',
+		[210] = 'Используется GET, где необходимо использовать POST',
+		[211] = 'Метод не найден',
+		[220] = 'Сервис временно недоступен, попробуйте чуть позже.',
+		[300] = 'Неправильный token (возможно истек срок действия, либо ваш IP изменился)',
+		[301] = 'Неправильный пароль, либо пользователь не найден',
+		[302] = 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс)',
+	
+	}
+	
+	local res,data = sms_client:cost(tArgs.to,get_msg())
+	if res then
+		print(('\nCost: %s\nSMS Count: %s\n'):format((data[2] and data[2] or '?'),(data[3] and data[3] or '?')))
+		os.exit(_EXIT_SUCCESS)
+	else
+		print('Error:',(_STATUSES[data] and _STATUSES[data] or 'Unable to get cost sms message. SMS.ru return code: '..data))
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+function sms_send()
+	if not tArgs.to or not tArgs.to:match("^%d+$") then
+		print('Invalid value for argument \'to\'.')
+		os.exit(_EXIT_ERRUSE)
+	end
+	local _STATUSES={
+		[100] = 'Сообщение принято к отправке. На следующих строчках вы найдете идентификаторы отправленных сообщений в том же порядке, в котором вы указали номера, на которых совершалась отправка.',
+		[200] = 'Неправильный api_id',
+		[201] = 'Не хватает средств на лицевом счету',
+		[202] = 'Неправильно указан получатель',
+		[203] = 'Нет текста сообщения',
+		[204] = 'Имя отправителя не согласовано с администрацией',
+		[205] = 'Сообщение слишком длинное (превышает 8 СМС)',
+		[206] = 'Будет превышен или уже превышен дневной лимит на отправку сообщений',
+		[207] = 'На этот номер (или один из номеров) нельзя отправлять сообщения, либо указано более 100 номеров в списке получателей',
+		[208] = 'Параметр time указан неправильно',
+		[209] = 'Вы добавили этот номер (или один из номеров) в стоп-лист',
+		[210] = 'Используется GET, где необходимо использовать POST',
+		[211] = 'Метод не найден',
+		[212] = 'Текст сообщения необходимо передать в кодировке UTF-8 (вы передали в другой кодировке)',
+		[220] = 'Сервис временно недоступен, попробуйте чуть позже.',
+		[230] = 'Сообщение не принято к отправке, так как на один номер в день нельзя отправлять более 100 сообщений.',
+		[300] = 'Неправильный token (возможно истек срок действия, либо ваш IP изменился)',
+		[301] = 'Неправильный пароль, либо пользователь не найден',
+		[302] = 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс)',
+	}
+	local res,errcode = sms_client:send(tArgs.to, get_msg(), tArgs.from, tArgs.time, tArgs.translit)
+	if res and errcode then
+		os.exit(_EXIT_SUCCESS)
+	elseif not res then
+		print('Error:',(_STATUSES[errcode] and _STATUSES[errcode] or 'Unable to send sms message. SMS.ru return code: '..errcode))
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+function get_balance()
+	local _STATUSES = {
+		[100] = 'Запрос выполнен. На второй строчке вы найдете ваше текущее состояние баланса.',
+		[200] = 'Неправильный api_id',
+		[210] = 'Используется GET, где необходимо использовать POST',
+		[211] = 'Метод не найден',
+		[220] = 'Сервис временно недоступен, попробуйте чуть позже.',
+		[300] = 'Неправильный token (возможно истек срок действия, либо ваш IP изменился)',
+		[301] = 'Неправильный пароль, либо пользователь не найден',
+		[302] = 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс)',
+	}
+	local res,data = sms_client:balance()
+	if res and data then
+		print(('Balance: %s'):format((data and data or '?')))
+		os.exit(_EXIT_SUCCESS)
+	else 
+		print('Error:',(_STATUSES[data] and _STATUSES[data] or 'Unable to get balance. SMS.ru return code: '..data))
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+function get_limit()
+	local _STATUSES = {
+		[100] = 'Запрос выполнен. На второй строчке вы найдете ваше текущее дневное ограничение. На третьей строчке количество сообщений, отправленных вами в текущий день.',
+		[200] = 'Неправильный api_id',
+		[210] = 'Используется GET, где необходимо использовать POST',
+		[211] = 'Метод не найден',
+		[220] = 'Сервис временно недоступен, попробуйте чуть позже.',
+		[300] = 'Неправильный token (возможно истек срок действия, либо ваш IP изменился)',
+		[301] = 'Неправильный пароль, либо пользователь не найден',
+		[302] = 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс)',
+	}
+	local res,code = sms_client:limit()
+	if res then
+		print(('\nDay limit: %s\nMessages (Count) sent today: %s'):format((code[2] and code[2] or '?'), (code[3] and code[3] or '?')))
+		os.exit(_EXIT_SUCCESS)
+	else 
+		print('Error:',(_STATUSES[code[1]] and _STATUSES[code[1]] or 'Unable to get limit. SMS.ru return code: '..code[1]))
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+function stoplist_add()
+	if not tArgs.to or not tArgs.to:match("^%d+$") then
+		print('Invalid value for argument \'to\'.')
+		os.exit(_EXIT_ERRUSE)
+	elseif not tArgs.message or type(tArgs.message) ~= 'string' then
+		print('Invalid value for argument \'message\'.')
+		os.exit(_EXIT_ERRUSE)
+	end
+	local res = sms_client:stoplist_add(tArgs.to, tArgs.message)
+	if res then
+		os.exit(_EXIT_SUCCESS)
+	else
+		print('Invalid phone number format')
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+function stoplist_del()
+	if not tArgs.to or not tArgs.to:match('^%d+$') then
+		print('Invalid value for argument \'to\'.')
+		os.exit(_EXIT_ERRUSE)
+	end
+	local res = sms_client:stoplist_del(tArgs.to)
+	if res then
+		os.exit(_EXIT_SUCCESS)
+	else 
+		print('Invalid phone number format')
+		os.exit(_EXIT_SMSRU_ERR)
+	end
+end
+
+if arg and type(arg) == 'table' then
 	main()
 end
